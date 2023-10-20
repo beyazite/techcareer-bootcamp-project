ceee05d76c7a8fd1b0f2d897386b12cf12dfa9ed
nevzatgokcen <nevzatgokcen99@gmail.com>
2023-10-20 19:13:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          TechCareer Mid Level Bootcamp Project T1
-        </p>
-        
+        <p>TechCareer Mid Level Bootcamp Project T1 ss</p>
       </header>
     </div>
   );
