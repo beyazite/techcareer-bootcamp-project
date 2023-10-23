a6aa3ec9a7da606cbf2fd78cee31a425c9f2da90
Burcu Ozdemir <burcuozdemir38@gmail.com>
2023-10-23 20:54:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import Navbar from "./components/shared/Navbar";
-import VisualSlide from "./components/shared/VisualSlide";
-import Filter from "./components/shared/Filter";
-import Subscribe from "./components/shared/Subscribe";
-import Footer from "./components/shared/Footer"
+
 
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <VisualSlide></VisualSlide>
-      <Filter></Filter>
-
       
-      <Subscribe></Subscribe>
-      <Footer></Footer>
     </div>
   );
 };
